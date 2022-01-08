e02176f5ab0fe62fef28c3a8fd77adcaa590f337
akhilmhdh <akhilmhdh@gmail.com>
2022-01-08 15:21:11 +0530

    feat: added font poppins and static images

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({
-    'src/_includes/assets/css/global.css': './global.css',
-  });
+  eleventyConfig.addPassthroughCopy('./src/static');
+  eleventyConfig.addPassthroughCopy('./src/css/fonts');
+
   return {
     dir: {
       input: 'src',
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   mode: 'jit',
   content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue,njk}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#385B48',
+        secondary: '#EAF1E4',
+      },
+    },
   },
   plugins: [],
 };
